e5fff17a49d9c2ad597072c96b2b8f94d123fc38
Okereke Chinweotito <okerekechinweotito@gmail.com>
2023-04-05 10:13:40 +0100

    upload

---
+++ script.js
@@ -0,0 +1,27 @@
+// utilities
+var get = function (selector, scope) {
+  scope = scope ? scope : document;
+  return scope.querySelector(selector);
+};
+
+var getAll = function (selector, scope) {
+  scope = scope ? scope : document;
+  return scope.querySelectorAll(selector);
+};
+
+// responsive navigation
+var topNav = get(".menu");
+var icon = get(".toggle");
+
+window.addEventListener("load", function () {
+  function showNav() {
+    if (topNav.className === "menu") {
+      topNav.className += " responsive";
+      icon.className += " open";
+    } else {
+      topNav.className = "menu";
+      icon.classList.remove("open");
+    }
+  }
+  icon.addEventListener("click", showNav);
+});
